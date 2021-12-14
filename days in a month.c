a9a4a92490304494d658fb7acb68279cffcb6a7e
Satvika7 <94847138+Satvika7@users.noreply.github.com>
2021-12-14 15:20:07 +0530

    Add files via upload

---
+++ days in a month.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int m;
+	scanf("%d",&m);
+	switch(m)
+	{
+		case 1:
+		case 3:
+	    case 5:
+	    case 7:
+	    case 8:
+	    case 10:
+	    case 12:printf("31 days");break;
+	    case 2:printf("28 days");break;
+	    default:printf("30 days");
+	}
+}
