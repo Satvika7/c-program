1ce9e168b42ea6bbe872a0bbc5dbd2e05f574b35
Satvika7 <94847138+Satvika7@users.noreply.github.com>
2022-01-03 21:42:33 +0530

    Add files via upload

---
+++ odd or even 1-10(switch).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+	    case 9:printf("odd");
+        break;
+        case 2:
+       	case 4:
+   		case 6:
+		case 8:
+        case 10:printf("even");
+        break;
+        default:printf("not valid");
+	}
+}
