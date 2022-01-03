41567416a013bfa3cdd3fa3db39f83fb0295eee1
Satvika7 <94847138+Satvika7@users.noreply.github.com>
2022-01-03 21:05:02 +0530

    Add files via upload

--- vowel(switch).c
+++ vowel(switch).c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	char c;
-	scanf("%c",&c);
-	switch(c)
+	char ch;
+	scanf("%c",&ch);
+	switch(ch)
 	{
-	case 'a':printf("vowel");break;
-    case 'e':printf("vowel");break;
-    case 'i':printf("vowel");break;
-    case 'o':printf("vowel");break;
-	case 'u':printf("vowel");break;
-	default :printf("consonant");
+		case 'a':printf("vowel");
+		         break;
+	    case 'e':printf("vowel");
+	            break;
+	    case 'i':printf("vowel");
+	            break;
+	    case 'o':printf("vowel");
+	            break;
+	    case 'u':printf("vowel");
+	            break;
+	    default:printf("consonent");
 	}
 }
