d30aa71fac80a53cbdc4563318ba1fdb5d5c368a
Satvika7 <94847138+Satvika7@users.noreply.github.com>
2022-02-07 10:47:50 +0530

    Add files via upload

---
+++ add(2d array).c
@@ -0,0 +1,46 @@
+#include<Stdio.h>
+int main()
+{
+	int a[3][3];
+	int i,j;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d",a[i][j]);
+		}
+	printf("\n");
+	}
+	int b[3][3];
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&b[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d",b[i][j]);
+		}
+	printf("\n");
+	}
+	int c[3][3];
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d   ",a[i][j]+b[i][j]);
+		}
+	printf("\n");
+	}
+}
---
+++ sum of diagonal(2d array).c
@@ -0,0 +1,26 @@
+#include<Stdio.h>
+int main()
+{
+	int a[3][3];
+	int i,j;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+    }
+    int sum=0;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+		if(i==j)
+     	{
+		sum=sum+a[i][j];
+	    
+		}
+		}
+	}
+		printf("%d",sum);
+}
